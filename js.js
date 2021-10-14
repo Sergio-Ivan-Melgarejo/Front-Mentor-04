85cf88b6638853fc624fc11df48768a94baa8523
Sergio <sergio.ivan.melgarejo@gmail.com>
2021-10-14 14:53:15 -0300

    i add details
    I upload "readme", screenshot, and i fix details

--- js.js
+++ js.js
@@ -1,40 +1,40 @@
+"use strict"
 const card2 = document.querySelector(".card__box-2");
-card2.addEventListener("click", (e)=>{
-    let elemento = e.target;
-    if(elemento.tagName === "IMG"){
-        elemento.parentElement.classList.toggle("active");
-        if(elemento.style.transform == "rotate(180deg)"){
-            elemento.style.transform = "";
-            elemento.parentElement.children[0].style.fontWeight = "";
-        }
-        else{
-            elemento.style.transform = "rotate(180deg)";
-            elemento.parentElement.children[0].style.fontWeight = "700";
-        }
-    }
-    else if(elemento.tagName === "DIV" && !elemento.classList.contains("card__box-2")){
-        elemento.classList.toggle("active");
-        if(elemento.children[1].style.transform == "rotate(180deg)"){
-            elemento.children[1].style.transform = "";
-            elemento.children[0].style.fontWeight = "";
-        }
-        else{
-            elemento.children[1].style.transform = "rotate(180deg)";
-            elemento.children[0].style.fontWeight = "700";
-        }
-    }
-    else if(elemento.tagName === "P"){
-        elemento.parentElement.classList.toggle("active");
 
-        if(elemento.parentElement.children[1].style.transform  === "rotate(180deg)"){
-                elemento.parentElement.children[1].style.transform = "";
-                elemento.parentElement.children[0].style.fontWeight = "";
+card2.addEventListener("click", (e)=>{
+    let element = e.target;
+    if(element.tagName === "IMG"){
+        element.parentElement.classList.toggle("active");
+        if(element.style.transform == "rotate(180deg)"){
+            element.style.transform = "";
+            element.parentElement.children[0].style.fontWeight = "";
+        }
+        else{
+            element.style.transform = "rotate(180deg)";
+            element.parentElement.children[0].style.fontWeight = "700";
+        }
+    }
+    else if(element.tagName === "DIV" && !element.classList.contains("card__box-2")){
+        element.classList.toggle("active");
+        if(element.children[1].style.transform == "rotate(180deg)"){
+            element.children[1].style.transform = "";
+            element.children[0].style.fontWeight = "";
+        }
+        else{
+            element.children[1].style.transform = "rotate(180deg)";
+            element.children[0].style.fontWeight = "700";
+        }
+    }
+    else if(element.tagName === "P"){
+        element.parentElement.classList.toggle("active");
+
+        if(element.parentElement.children[1].style.transform  === "rotate(180deg)"){
+                element.parentElement.children[1].style.transform = "";
+                element.parentElement.children[0].style.fontWeight = "";
             }
             else{
-                elemento.parentElement.children[1].style.transform  = "rotate(180deg)";
-                elemento.parentElement.children[0].style.fontWeight = "700";
+                element.parentElement.children[1].style.transform  = "rotate(180deg)";
+                element.parentElement.children[0].style.fontWeight = "700";
             }
     }
 });
-
-
