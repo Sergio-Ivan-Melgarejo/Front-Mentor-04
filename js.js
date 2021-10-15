412dacf33dc6c3c9b0e6454b826f6592402dc1da
Sergio <sergio.ivan.melgarejo@gmail.com>
2021-10-15 09:15:19 -0300

    i change details
    i change my name in the link and i add trancition

--- js.js
+++ js.js
@@ -1,40 +1,39 @@
 "use strict"
-const card2 = document.querySelector(".card__box-2");
+const box2 = document.querySelector(".card__box-2");
 
-card2.addEventListener("click", (e)=>{
+box2.addEventListener("click", (e)=>{
     let element = e.target;
     if(element.tagName === "IMG"){
         element.parentElement.classList.toggle("active");
-        if(element.style.transform == "rotate(180deg)"){
+        if(element.style.transform == "rotate(-180deg)"){
             element.style.transform = "";
             element.parentElement.children[0].style.fontWeight = "";
         }
         else{
-            element.style.transform = "rotate(180deg)";
+            element.style.transform = "rotate(-180deg)";
             element.parentElement.children[0].style.fontWeight = "700";
         }
     }
     else if(element.tagName === "DIV" && !element.classList.contains("card__box-2")){
         element.classList.toggle("active");
-        if(element.children[1].style.transform == "rotate(180deg)"){
+        if(element.children[1].style.transform == "rotate(-180deg)"){
             element.children[1].style.transform = "";
             element.children[0].style.fontWeight = "";
         }
         else{
-            element.children[1].style.transform = "rotate(180deg)";
+            element.children[1].style.transform = "rotate(-180deg)";
             element.children[0].style.fontWeight = "700";
         }
     }
     else if(element.tagName === "P"){
         element.parentElement.classList.toggle("active");
-
-        if(element.parentElement.children[1].style.transform  === "rotate(180deg)"){
+        if(element.parentElement.children[1].style.transform  === "rotate(-180deg)"){
                 element.parentElement.children[1].style.transform = "";
                 element.parentElement.children[0].style.fontWeight = "";
             }
-            else{
-                element.parentElement.children[1].style.transform  = "rotate(180deg)";
-                element.parentElement.children[0].style.fontWeight = "700";
-            }
+        else{
+            element.parentElement.children[1].style.transform  = "rotate(-180deg)";
+            element.parentElement.children[0].style.fontWeight = "700";
+        }
     }
 });
